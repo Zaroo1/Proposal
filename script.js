37b01037736dd8e862657cec9a281503bcd1760c
Abdul-Aziz Zaroo <abdulazizzaroo@gmail.com>
2025-04-09 17:23:26 +0000

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ function showAnswer(answer) {
   const answerDiv = document.getElementById('answer');
   // Check if she clicked 'Yes'
   if (answer === 'yes') {
-    answerDiv.innerHTML = "She said YES! 💖 Let's grow together and see where life takes us!";
+    answerDiv.innerHTML = "Thank you for saying YES! 💖 Let's grow together and see where life takes us!";
     
     // Send email notification when "Yes" is selected
     sendEmailNotification();
   } else {
-    answerDiv.innerHTML = "No matter what, I'll always cherish you. ❤️";
+    answerDiv.innerHTML = "So sad. But know that oo matter what, I'll always cherish you. ❤️";
   }
   // Disable the buttons after an answer to avoid further clicks
   document.getElementById('yesButton').disabled = true;
@@ -22,14 +22,14 @@ function showAnswer(answer) {
 // Send email notification
 function sendEmailNotification() {
   const templateParams = {
-    to_name: "Your Name", // Replace with your name
+    to_name: "Zaroo", // Replace with your name
     from_name: "Naima", // Replace with Naima's name
     message: "Naima said YES to the proposal!",
     reply_to: "abdulazizzaroo@gmail.com" // Your email address
   };
 
   // Send the email using EmailJS
-  emailjs.send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", templateParams)
+  emailjs.send("Zaroo_aziz", "template_uknkz69", templateParams)
     .then(function(response) {
        console.log('Success:', response);
     }, function(error) {
